8558836dacc01d3c329ddb603af940d099c476af
Rawan <r.k096@hotmail.com>
2023-03-18 16:03:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, word){
+    const arr = []
+    for(const n of names){
+        arr.push( `Thank you, ${n}, for the wonderful ${word} gift!`)
+    }
+    return arr
+}
+
+function countDown(num) {
+
+    for (let i =num; i>=0; i--){
+        console.log(i)
+    }
+}
